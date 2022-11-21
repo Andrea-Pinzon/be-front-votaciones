719443793bfe3e841f98dc388edbbbf05c4549ca
Andrea-Pinzon <71514080+Andrea-Pinzon@users.noreply.github.com>
2022-11-21 14:39:57 -0500

    Candidatos

--- src/app/pages/candidatos/candidatos.component.ts
+++ src/app/pages/candidatos/candidatos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CandidatosService } from 'src/app/servicios/candidatos.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -7,23 +8,48 @@ import Swal from 'sweetalert2';
   styleUrls: ['./candidatos.component.css']
 })
 export class CandidatosComponent implements OnInit {
-
-  constructor() { }
+  candidato : any;
+  nombresColumnas: string[] = ['#Resolucion','Cedula','Nombre','Apellido'];
+  constructor(private miServicioCandidatos: CandidatosService) { }
 
   ngOnInit(): void {
-    // this.listar();
+    this.listar();
+  }
+  listar():void{
+    this.miServicioCandidatos.listar().
+      subscribe(data => {
+        this.candidato=data;
+    });
   }
 
   agregar():void{
-    console.log("agregando nuevo");
+    this.router.navigate(['/dashboard/crear']);
   }
 
-  editar():void{
-    // console.log("editando a "+id);
+  editar(id: string):void{
+    this.router.navigate(['/dashboard/actualizar/' +id]);
   }
 
-  eliminar(){
-
+  eliminar(id:string):void{
+    Swal.fire({
+      title: 'Eliminar Candidato',
+      text: "Está seguro que quiere eliminar este candidato?",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.miServicioCandidatos.eliminar(id).subscribe(data => {
+          Swal.fire(
+            'Eliminado!',
+            'El Candidato ha sido eliminado correctamente',
+            'success'
+          );
+          this.ngOnInit();
+        });
+      }
+    })
   }
-
 }
