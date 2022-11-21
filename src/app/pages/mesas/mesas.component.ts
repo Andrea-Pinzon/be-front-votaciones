24b9bf6655e807381c806dec83166adbfab5640f
Andrea-Pinzon <71514080+Andrea-Pinzon@users.noreply.github.com>
2022-11-21 15:01:03 -0500

    Mesas

--- src/app/pages/mesas/mesas.component.ts
+++ src/app/pages/mesas/mesas.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MesasService } from 'src/app/servicios/mesas.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-mesas',
@@ -6,21 +8,50 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./mesas.component.css']
 })
 export class MesasComponent implements OnInit {
+  mesas : any;
+  nombresColumnas: string[] = ['Mesas','Cedulas inscritas'];
 
-  constructor() { }
+  constructor(private miServicioMesas: MesasService) { }
 
   ngOnInit(): void {
+    this.listar();
+  }
+  listar():void{
+    this.miServicioMesas.listar().
+      subscribe(data => {
+        this.mesas=data;
+    });
   }
 
-  editar(){
-
-  }
-  eliminar(){
-
+  agregar():void{
+    // this.router.navigate(['/dashboard/crear']);
   }
 
-  nuevo(){
-
+  editar(id: string):void{
+    // this.router.navigate(['/dashboard/actualizar/' +id]);
   }
 
- }
+
+  eliminar(id:string):void{
+    Swal.fire({
+      title: 'Eliminar Mesa',
+      text: "Está seguro que quiere eliminar esta mesa?",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.miServicioMesas.eliminar(id).subscribe(data => {
+          Swal.fire(
+            'Eliminado!',
+            'La mesa ha sido eliminada correctamente',
+            'success'
+          );
+          this.ngOnInit();
+        });
+      }
+    })
+  }
+}
