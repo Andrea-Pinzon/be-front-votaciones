4124238ea31881cf2cc6ad55311802386d509504
Andrea-Pinzon <71514080+Andrea-Pinzon@users.noreply.github.com>
2022-11-21 14:04:13 -0500

    Servicios Mesas y Candidatos

--- src/app/servicios/candidatos.service.ts
+++ src/app/servicios/candidatos.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Candidatos } from '../modelos/candidatos.model';
+import { Usuario } from '../modelos/usuario.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CandidatosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  listar(): Observable<Candidatos[]> {
+      return this.http.get<Candidatos[]>(`${environment.url_gateway}/candidatos`);
+  }
+
+  eliminar(id:string){
+      return this.http.delete<Candidatos>(`${environment.url_gateway}/candidatos/${id}`,);
+  }
+
 }
--- src/app/servicios/mesas.service.ts
+++ src/app/servicios/mesas.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Mesas } from '../modelos/mesas.model';
+import { Usuario } from '../modelos/usuario.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MesasService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  listar(): Observable<Mesas[]> {
+      return this.http.get<Mesas[]>(`${environment.url_gateway}/mesas`);
+  }
+
+  eliminar(id:string){
+      return this.http.delete<Mesas>(`${environment.url_gateway}/mesas/${id}`,);
+  }
+
 }
